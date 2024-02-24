11481294f48cc0108007ce775c92fd47a2113ab4
Charles Patel <charlespatel007@yahoo.com>
2024-02-24 12:37:41 -0600

    Add intial logging support

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import logging
 from argparse import ArgumentParser
 from pathlib import Path
 
@@ -5,12 +6,24 @@ from src.files import Script
 from src.types import ConfigFile
 
 
+def setup_logger() -> None:
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
 def main() -> None:
 
     parser = ArgumentParser()
     parser.add_argument("file", help="Your project's setup config file", type=ConfigFile)
     args = parser.parse_args()
-
+    setup_logger()
     cfg_file = args.file
     filepath = Path(".").resolve() / cfg_file.value
 
